2411ce51d9612e97ec423b379701359d535c73b8
Matheus Campanhola Santos <80859669+matheuscsant@users.noreply.github.com>
2024-08-12 15:49:10 -0300

    Setup project and example

---
+++ build.sbt
@@ -0,0 +1,20 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.3.3"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "Simple-Api-Scala-With-Akka"
+  )
+
+val AkkaVersion = "2.9.3"
+val AkkaHttpVersion = "10.6.3"
+
+resolvers += "Akka library repository".at("https://repo.akka.io/maven")
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+)
---
+++ src/main/scala/main.scala
@@ -0,0 +1,53 @@
+
+// akka
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+// akka http
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.*
+// spray
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport.*
+import spray.json.DefaultJsonProtocol.*
+
+final case class User(id: Long, name: String, email: String)
+
+// Simple example of Scala API
+@main def httpserver(): Unit = {
+  implicit val actorSystem: ActorSystem[Any] = ActorSystem(Behaviors.empty, "akka-http")
+  implicit val userMarshaller: spray.json.RootJsonFormat[User] = jsonFormat3(User.apply)
+
+  val getUser = get {
+    path("user" / LongNumber) {
+      println("Getting user")
+      userId =>
+        complete(User.apply(userId, "Test-user", "test@test.com"))
+    }
+  }
+
+  val createUser = post {
+    path("user") {
+      entity(as[User]) {
+        println("Creating user")
+        user => complete(user)
+      }
+    }
+  }
+
+  val updateUser = put {
+    path("user" / LongNumber) { userId =>
+      entity(as[User]) {
+        println("Updating user")
+        user => complete(User(userId, user.name, user.email))
+      }
+    }
+  }
+
+  val deleteUser = delete {
+    path("user" / LongNumber) {
+      println("Deleting user")
+      userId => complete(User(userId, "DeleteTestUser", "test@teste.com"))
+    }
+  }
+  Http().newServerAt("localhost", 8080).bind(concat(getUser, createUser, updateUser, deleteUser))
+}
+
